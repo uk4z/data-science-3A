9fd7584f2ebed44b6e57caa1fa0ffa100f6c3f44
uk4z <ulyssekaz@gmail.com>
2025-04-16 20:12:32 +0200

    some work on data ingestion

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ from loguru import logger
 from components.sidebar import render_sidebar
 from components.conversation import render_conversation
 import json
+import s3fs
+
+
+fs = s3fs.S3FileSystem(client_kwargs={"endpoint_url": "https://minio.lab.sspcloud.fr"})
+MY_BUCKET = "ukazmierczak"
+PATH = "base.json"
 
 # Configuration du logger pour écrire dans un fichier
 logger.add("application.log", rotation="500 KB", retention="10 days", level="DEBUG")
+
 
 def load_css(file_name):
     with open(file_name) as f:
@@ -13,28 +20,37 @@ def load_css(file_name):
     st.markdown(f"<style>{css_content}</style>", unsafe_allow_html=True)
     logger.info(f"CSS chargé depuis '{file_name}'")
 
-# Chargement du fichier base.json
-try:
-    with open("data/base.json", "r") as f:
-        data = json.load(f)
-    logger.info("Fichier 'base.json' chargé avec succès")
-except Exception as e:
-    logger.error("Erreur lors du chargement de 'base.json'")
-    raise e
 
-# Initialisation de la session et création de session.json si première exécution
-if "is_first_load" not in st.session_state:
+# Chargement du fichier base.json
+def load_data():
     try:
-        with open("data/session.json", "w") as f:
-            json.dump(data, f, indent=4)
-        st.session_state["is_first_load"] = True
-        logger.info("Session initialisée et 'session.json' créé")
+        with fs.open(f"s3://{MY_BUCKET}/{PATH}", "r") as f:
+            return json.load(f)
     except Exception as e:
-        logger.error("Erreur lors de l'écriture de 'session.json'")
-        raise e
+        logger.error(f"Error loading data: {e}")
+        return {}
+
+
+def save_data(data):
+    try:
+        with fs.open(f"s3://{MY_BUCKET}/{PATH}", "w") as f:
+            json.dump(data, f, indent=4)
+        logger.info("Data successfully written to S3")
+    except Exception as e:
+        logger.error(f"Error saving data: {e}")
+
+
+st.session_state.data = load_data()
+
+if "is_first_load" not in st.session_state:
+    st.session_state.is_first_load = True
+elif st.session_state.data:
+    logger.info("Condition met")
+    save_data(st.session_state.data)
+
 
 load_css("main.css")
 logger.info("Affichage de la sidebar")
-render_sidebar(data.keys())
+render_sidebar(st.session_state.data.keys())
 logger.info("Affichage de la conversation")
 render_conversation()
